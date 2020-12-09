361e18d740b4b29a196c5d8aba8224cc35098456
91950 <sharathkumarreddy24@gmail.com>
2020-12-09 12:15:58 +0530

    First commit

---
+++ Variables/Info.py
@@ -0,0 +1,21 @@
+# Variable is a name which is used to reffer memory location.
+
+name = 'sarath'    # here name is a variable
+print(name)
+
+''''
+Python is a dynamically typed programing language hence we can not remember the data type.
+variable name starts with alphabet only
+variable can contain alphabets and numbers.
+_ private variable
+__strongly private
+__ to __ special variables
+'''
+
+a = 10
+print(a)
+
+msg = "hello"
+print(msg)
+
+
