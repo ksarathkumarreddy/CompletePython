57e5f6cf797c96563518a84a039dc7098561052f
91950 <sharathkumarreddy24@gmail.com>
2020-12-12 15:32:36 +0530

    Class variables and class methods

--- 6_OOPs/3_CV_CM.py
+++ 6_OOPs/3_CV_CM.py
@@ -1,8 +1,29 @@
 '''
 1.Class variables can access Class methods.
-2.Class variables can't access Instance methods.
-3.Instance variables can access Class methods.
+2.Class variables can access Instance methods.
+3.Instance variables can't access Class methods.
+
+CV CM   YES
+IV IM   YES
+CV IM   YES
+IV CM   NO
+
 
 '''
 # Class variables and methods
+class Emp:
+    office = 'CGI'
+    @classmethod
+    def get_info(cls):
+       print("Class Emp data:",cls.office)
+Emp.get_info()
+
+
+class Stu:
+    clg_address = 'mpl'
+    clg_name = 'Ganambica'
+    @classmethod
+    def get_add(cls):
+        print("Student college address:",cls.clg_address,cls.clg_name)
+Stu.get_add()
 
