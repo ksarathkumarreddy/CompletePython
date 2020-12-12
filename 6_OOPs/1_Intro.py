5a5d2f6fdc138985756d36557b095fbec049c668
91950 <sharathkumarreddy24@gmail.com>
2020-12-12 18:05:48 +0530

    Introduction about oops

--- 6_OOPs/1_Intro.py
+++ 6_OOPs/1_Intro.py
@@ -20,6 +20,17 @@ Behaviour:
 Instance Methods
 Class Methods
 
+
+Constructor:
+----
+It is a special operator in python.
+__init__ is an constructor
+It is used to intialize the instance variable and methods.
+Its an optinal because default constructor will be there.
+In __init__ inside one special constructor will be there which is called __new__.
+Where as __new__ can be used to create an object.
+__init__ can be intialized to an object.
+
 '''
 class Employee:
     # State
