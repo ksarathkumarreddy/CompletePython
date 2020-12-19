aa1e076a8ee8858499605eaaf03a84a720929277
91950 <sharathkumarreddy24@gmail.com>
2020-12-19 15:03:17 +0530

    sample file

--- 7_Exception Handling/2_Sample.py
+++ 7_Exception Handling/2_Sample.py
@@ -5,7 +5,7 @@ Exception is an event, that distrupts the normal flow of the program..
 
 try:
     fh = open('testfile','w')
-    fh.write("This is my test file for exception handling")
+    fh.write("This is a file for handling exception")
 except IOError:
     print("cannot find file or read data")
 else:
